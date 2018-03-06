9f831acdf62f6dd7ec7d0c06f79044f55d8890bd
czarstark <trevor.balint@gmail.com>
2018-03-06 21:47:49 +0000

    Modify how errors are handled (return error in place of weather). Add parsing in for Avg temperature. Clean code for use in main HAM function

--- Weather/getWeather.py
+++ Weather/getWeather.py
@@ -8,68 +8,65 @@ def getWeather(CITY, COUNTRY, MONTH):
   webCommand = "http://www.holiday-weather.com/" + CITY + "/averages/" + MONTH + "/"
   outString = ""
 
+  ## retrieve the webpage
   try:
     response = get(webCommand, stream=True)
     webContent = response.content
 
   except RequestException as error:
-    print('Error during requests to {0} : {1}'.format(webCommand, str(error)))
-    return "No weather for you!"
+    return 'Error during requests to {0} : {1} \nNo weather for you!'.format(webCommand, str(error))
 
-
+  ## create the parser
   try:
     soup = BeautifulSoup(webContent, 'html.parser')
 
   except:
-    print("Failure to convert to minestrone")
-    return "No weather for you!"
+    return "Failure to convert to minestrone. No weather for you!"
 
-  ## Check the country
+
+  ## Grab the title
   pageTitle = soup.title.get_text()
 
+  ## Check to see if the country matches (url uses city only)
   if COUNTRY.lower() not in pageTitle.lower():
     print("Incorrect Country?")
-
 
 
   outString += pageTitle
   outString += "\n\n"
 
-  for match in soup.find_all("ul", class_="destination-info"):
-    matchText = (match.get_text()).split("\n")
-    while "" in matchText:
-      matchText.remove("")
+  ## Find the parent object
+  match = soup.find("ul", class_="destination-info")
 
+  ## Extract the text from the child objects, remove empty lines
+  matchText = (match.get_text()).split("\n")
+  while "" in matchText:
+    matchText.remove("")
 
-    for index in range(len(matchText)):
-      if matchText[index] == "High Temperature":
-        outString += "High Temperature {}   ({})\n".format(matchText[index+2], matchText[index+1])
+  ## Parse content, based on values previously seen
+  for index in range(len(matchText)):
+    if matchText[index] == "Temperature":
+      outString += "Avg Temperature  {}   ({})\n".format(matchText[index+2], matchText[index+1])
 
-      elif matchText[index] == "Low Temperature":
-        outString += "Low Temperature  {}   ({})\n".format(matchText[index+2], matchText[index+1])
+    elif matchText[index] == "High Temperature":
+      outString += "High Temperature {}   ({})\n".format(matchText[index+2], matchText[index+1])
 
-      elif matchText[index] == "Sea Temperature":
-        outString += "Sea Temperature  {}   ({})\n".format(matchText[index + 2], matchText[index+1])
+    elif matchText[index] == "Low Temperature":
+      outString += "Low Temperature  {}   ({})\n".format(matchText[index+2], matchText[index+1])
 
-      elif matchText[index] == "Sunshine Hours":
-        outString += "Sunshine Hours   {}\n".format(matchText[index+1])
+    elif matchText[index] == "Sea Temperature":
+      outString += "Sea Temperature  {}   ({})\n".format(matchText[index + 2], matchText[index+1])
 
-      elif matchText[index] == "Chance Of Rain":
-        outString += "Chance of Rain   {}\n".format(matchText[index+1])
+    elif matchText[index] == "Sunshine Hours":
+      outString += "Sunshine Hours   {}\n".format(matchText[index+1])
 
-      elif matchText[index] == "Rainfall days":
-        outString += "Rainfall Days    {}\n".format(matchText[index+1])
+    elif matchText[index] == "Chance Of Rain":
+      outString += "Chance of Rain   {}\n".format(matchText[index+1])
 
-      elif matchText[index] == "Chance Of Cloudy Day":
-        outString += "Chance of Clouds {}\n".format(matchText[index + 1])
+    elif matchText[index] == "Rainfall days":
+      outString += "Rainfall Days    {}\n".format(matchText[index+1])
+
+    elif matchText[index] == "Chance Of Cloudy Day":
+      outString += "Chance of Clouds {}\n".format(matchText[index + 1])
 
   return outString
-
-
-## these will be inputs to HAM
-CITY = "Palermo"
-COUNTRY = "Italy"
-MONTH = "March"
-weather = getWeather(CITY, COUNTRY, MONTH)
-
-print(weather)
