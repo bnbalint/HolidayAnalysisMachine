0ef16927f00e47c54724961ad6bc99f4fa6f2a16
czarstark <trevor.balint@gmail.com>
2018-03-07 18:24:58 +0000

    getPopulation function is done Gets the size of the city and its rank within the country Also added a function to soupUtils that creates a dataframe from a table

--- Utils/getPopulation.py
+++ Utils/getPopulation.py
@@ -1,4 +1,5 @@
-from Utils.soupUtils import getSoup
+from Utils.soupUtils import getSoup, parse_html_table
+import pandas as pd
 
 
 def getPopulation(CITY, COUNTRY):
@@ -8,20 +9,22 @@ def getPopulation(CITY, COUNTRY):
 
     try:
         soup = getSoup(webCommand)
-
     except Exception as error:
         return 'Error during requests to {0} : {1} \nNo population for you!'.format(webCommand, str(error))
 
     # Find the object
-    match = soup.find(id="citysection")
-    for row in list(filter(lambda x: x != "\n", match.tbody.children)):
-        # Get all city names out of this row of the table
-        cityNames = [x.next for x in filter(lambda x: x != "\n", row.contents[1].find_all("span"))]
-        for city in cityNames:
-            if str.count(str.lower(city), str.lower(CITY)) > 0:
-                outString += "Population of " + CITY + " is " + row.contents[-3].next
-                return outString
-    return "Error - city not found in population table!"
+    df = parse_html_table(soup.find_all('table')[2]).set_index("Name")
+    popColumn = df.columns[len(df.columns)-3]  # In case there is a different population column for different countries
+    try:
+        cityPop = df[df.index.str.contains(CITY)][popColumn]  # This is complex since city can have multiple names
+        outString += "Population of " + CITY + " is " + str(max(cityPop))
+    except:
+        return "Error - city not found in population table!"
+
+    df.sort_values(by=popColumn, ascending=False, inplace=True)
+    valueInCountry = min([df.index.tolist().index(i) for i in df.index.tolist() if CITY in i]) + 1
+    outString += "\nRanking of " + str(valueInCountry) + " overall in " + COUNTRY
+    return outString
 
 
-print(getPopulation("Alicante", "Spain"))
+print(getPopulation("Naples", "Italy"))
--- Utils/soupUtils.py
+++ Utils/soupUtils.py
@@ -2,22 +2,76 @@
 from bs4 import BeautifulSoup
 from requests import get
 from requests.exceptions import RequestException
+import pandas as pd
 
 
+# webRequest is a url from which to generate a Soup
 def getSoup(webRequest):
-    ## retrieve the webpage
+    # retrieve the webpage
     try:
         response = get(webRequest, stream=True)
         webContent = response.content
-
     except RequestException as error:
         return error
 
-    ## create the parser
+    # create the parser
     try:
         soup = BeautifulSoup(webContent, 'html.parser')
-
     except Exception as error:
         return error
 
     return soup
+
+
+# Argument "table" is the HTML element for the table, normally found with soup.find_all('table')[n]
+# Returns a pandas dataframe
+def parse_html_table(table):
+    n_columns = 0
+    n_rows = 0
+    column_names = []
+
+    # Find number of rows and columns
+    # we also find the column titles if we can
+    for row in table.find_all('tr'):
+
+        # Determine the number of rows in the table
+        td_tags = row.find_all('td')
+        if len(td_tags) > 0:
+            n_rows += 1
+            if n_columns == 0:
+                # Set the number of columns for our table
+                n_columns = len(td_tags)
+
+        # Handle column names if we find them
+        th_tags = row.find_all('th')
+        if len(th_tags) > 0 and len(column_names) == 0:
+            for th in th_tags:
+                column_names.append(th.get_text())
+
+    # Safeguard on Column Titles
+    if len(column_names) > 0 and len(column_names) != n_columns:
+        raise Exception("Column titles do not match the number of columns")
+
+    columns = column_names if len(column_names) > 0 else range(0, n_columns)
+    df = pd.DataFrame(columns=columns,
+                      index=range(0, n_rows))
+    row_marker = 0
+    for row in table.find_all('tr'):
+        column_marker = 0
+        columns = row.find_all('td')
+        for column in columns:
+            if len(column.get_text()) > 0:
+                df.iat[row_marker, column_marker] = column.get_text().strip().replace(",","")
+            else: df.iat[row_marker, column_marker] = ""
+            column_marker += 1
+        if len(columns) > 0:
+            row_marker += 1
+
+    # Convert to float if possible
+    for col in df:
+        try:
+            df[col] = df[col].astype(float)
+        except ValueError:
+            pass
+
+    return df
